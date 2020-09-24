409a5708f50116ac45f145fc00fb67f2a08e3a10
Dirección de Bienestar Universitario <71483737+bienestaruai@users.noreply.github.com>
2020-09-23 20:39:58 -0500

    Add files via upload

---
+++ apoyo_economico.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="s>tylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!--link rel="stylesheet" href="css/custom.css"-->    
+    
+</head>
+<body>
+    <!-- Inscrustamos la cabecera -->
+    <?php require_once('header.php');?>
+
+    <div class="container section">
+      <center><h3 class="header">APOYO ECONÓMICO</h3></center>      
+    </div>
+    
+    <div class="container section">
+        <h4 >ADJUNTAR LOS REQUISITOS:</h4>
+        <div class="card-panel">
+            <h5>1. Constancia de clasificacion socieconomica segun SISFOH: <a  href="https://operaciones.sisfoh.gob.pe/cse/">https://operaciones.sisfoh.gob.pe/cse/</a></h5>
+            
+            <h5>2. Ficha de Matrícula del periodo 2020-2.</h5>
+            <h5>3. Boleta de Notas del semestre 2020-1 (El estudiante deberá de obtener un promedio ponderado mayor o igual a 15.</h5>
+            <h5>4. Documento que acredite el cumplimiento del pago de todas la pensiones (estado de cuenta o voucher quinta cuota).</h5>
+            <h5>5. Constancia del Seguro contra accidentes (vigente/ ESSALUD/ SIS/ Seguro Particular).</h5>
+            <h5>6. Copia del carnet de estudiante.</h5>
+            <h5>7. Voucher de pago derecho a Apoyo Económico (S/.15.00).</h5>
+            <h5>8. Copia del DNI.</h5>
+        </div>
+      
+        <a class="waves-effect waves-light#0288d1 light-blue darken-2 btn" href="carpeta_recategorizacion.jpg">POSTULA AQUI</a>
+        </br></br>
+    </div>
+
+    <?php require_once('footer.php');?>
+</body>
+</html>
+
+
+
+
+
